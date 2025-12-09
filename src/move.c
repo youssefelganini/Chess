ee956561a8343a9995793d91d777954407765aef
Youssefelganini <yosseflegnaini8@gamil.com>
2025-12-09 23:00:20 +0200

    all moves logic are partialy done still need fn for check stilll needs fn for the destination

--- src/move.c
+++ src/move.c
@@ -4,62 +4,141 @@
 int flag=0;
 int counter=0;
 
-bool rook(char *x){ /*input=[a,3,a,4]*/
-    if(x[0]==x[2]&&x[1]!=x[3]) return true;
-    if(x[0]!=x[2]&&x[1]==x[3]) return true;
+
+/*x is the input
+a  has the values of the input as int
+pawn logic
+destantion check
+checkoftheking
+checkmate
+*/
+
+
+
+int input_as_int[4];  // global array
+
+void convert_input_to_int(char *x) {
+    input_as_int[0]=x[0] - 'a'; // start column
+    input_as_int[1]=x[1] - '1'; // start row
+    input_as_int[2]=x[2] - 'a'; // end column
+    input_as_int[3]=x[3] - '1'; // end row
+}
+
+int sign(int x){ /*to know the piece go forward or backward*/
+    if(x>0) return 1;
+    if(x<0) return -1;
+    return 0;
+}
+bool rook(void){ /*input=[a,3,a,4]*/
+    if(input_as_int[0]==input_as_int[2]&&input_as_int[1]!=input_as_int[3]) return true;
+    if(input_as_int[0]!=input_as_int[2]&&input_as_int[1]==input_as_int[3]) return true;
     return false;
 }
-bool king(char *x){
-    int dx[]={1,1,1,0,0,-1,-1,-1};
-    int dy[]={1,0,-1,-1,-1,0,1,1};
-    for (int i=0;i<8;i++){
-        if((x[0]+dx[i]==x[2])&&(x[1]+dy[i]==x[3])) return true;
+bool king(void){
+    if(abs(input_as_int[0]-input_as_int[2])<=1 &&
+        abs(input_as_int[1]-input_as_int[3])<=1) return true;
+
+    return false;
+}
+
+bool bishop(void){
+    if(abs(input_as_int[2]-input_as_int[0])==abs(input_as_int[3]-input_as_int[1])) return true;
+    return false;
+}
+
+
+bool knight(void){
+if(((abs(input_as_int[0]-input_as_int[2])==2) && (abs(input_as_int[1]-input_as_int[3])==1)) || 
+   ((abs(input_as_int[0]-input_as_int[2])==1) && (abs(input_as_int[1]-input_as_int[3])==2))) return true;
+   return false;
+}
+bool queen(void){
+    if(rook()||bishob()) return true;
+    return false;
+}
+bool path_check(char board[8][8]){
+    int dx=sign(input_as_int[2]-input_as_int[0]);
+    int dy=sign(input_as_int[3]-input_as_int[1]);
+    int x=dx+input_as_int[0];
+    int y=dy+input_as_int[1];
+    while (x!=input_as_int[2]||y!=input_as_int[3])
+    {
+        if(board[y][x]!='.' && board[y][x]!='-') return false;
+        x+=dx;
+        y+=dy;
     }
-    return false;
+    return true;
 }
-bool bishob(char *x){
-    if((x[0]-x[2]==x[1]-x[3])||(x[2]-x[0]==x[1]-x[3])||
-        (x[0]-x[2]==x[3]-x[1])||(x[2]-x[0]==x[3]-x[1])) return true;
-    return false;
-}
-bool knight(char *x){
-    int a[4];
-    a[0]=(int)x[0];
-    a[1]=(int)x[1];
-    a[2]=(int)x[2];
-    a[3]=(int)x[3];
-    if(((abs(a[0]-a[2])==2) && (abs(a[1]-a[3])==1)) || 
-    ((abs(a[0]-a[2])==1) && (abs(a[1]-a[3])==2))) return true;
-    return false;
-}
-bool queen(char *x){
-    if(rook(x/*input*/)||bishob(x)) return true;
-    return false;
+bool is_king_checked(){
+
 }
 bool validation(char *x,char board[8][8]){
-    int a[4];
-    a[0]=(int)(x[0]-'a');
-    a[1]=(int)(x[1]-'1');
-    a[2]=(int)(x[2]-'a');
-    a[3]=(int)(x[3]-'1');
-
-    char piece=board[a[0]][a[1]];
+    char piece=board[input_as_int[1]][input_as_int[0]];
     if(x[0]==x[2]&&x[1]==x[3]) {
         printf("Invlaid input ,you didnot move the piece\n");
         return false;}
     if(x[0]<'a' || x[0]>'h' || x[2]<'a' || x[2]>'h' || 
             x[1]<'1' || x[1]>'8' || x[3]<'1' || x[3]>'8') {
-        printf("Invalid input, must be (a-h) and (1-8)\n");
+        printf("Invalid input, must be in the range of (a-h) and (1-8)\n");
         return false;
     }
     switch (piece)
     {
-    case constant expression:
-        /* code */
-        break;
+    case 'r':
+    case 'R':
+        if(path_check(/*last board*/)==false){
+           printf("Path is blocked\n");
+            return false;
+        }
+        return rook();
+    case 'b':
+    case 'B':
+        if(path_check(/*last board*/)==false){
+            printf("Path is blocked\n");
+            return false;
+        }
+        return bishop();
+    case 'Q':
+    case 'q':
+        if(path_check(/*last board*/)==false){
+            printf("Path is blocked\n");
+            return false;
+        }
+        return queen();
+    case 'K':
+    case 'k':
+        return king();
+    case 'N':
+    case 'n':
+        return knight();
+    case 'P':
+        if(x[1]=='2'){
+            if(x[3]=='4'){
+                if(path_check(/*last board*/)==false){
+                    printf("Path is blocked\n");
+                    return false;
+        }
+            return true;                
+            }
+            
+        }
+        if(x[3]==x[1]+'1') return true;
+        
+    case 'p':
+        if(x[1]=='7'){
+        if(x[3]=='5'){
+                if(path_check(/*last board*/)==false){
+                    printf("Path is blocked\n");
+                    return false;
+        }
+            return true;                
+            }
+        }
+        if(x[3]==x[1]-'1') return true;
     
     default:
-        break;
+        pritf("Invalid input,This square has no pieces\n");
+        return false;
     }
 
 }
