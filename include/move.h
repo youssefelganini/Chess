e70b02a31257fd1da545be575398659908040ff9
Youssefelganini <yosseflegnaini8@gamil.com>
2025-12-18 14:25:22 +0200

    promation is done and header file

--- include/move.h
+++ include/move.h
@@ -6,6 +6,17 @@ int sign(int x);
 int rook();
 int king();
 int bishop();
+int knight();
+int queen();
+int path_check(Board *board);
+int des_check(Board *board);
+int castling_path_clear(Board *board,int king_row,int king_col,int rook_col);
+int castling(Board *board,char *castl);
+int pawn_promotion(Board *board);
+int change_pawn(Board *board)
+int is_king_checked(Board *board);
+int validation(char *x,Board *board)
+void execute_move(Board *board,int from_row, int from_col,int to_row,int to_col);
 
 
 
